eb8f65016ceaed010da87145c5f075c07a6d154c
AndreyNesterov <1@nestandre.ru>
2022-09-07 00:30:39 +0300

    v 1.3

--- task4.py
+++ task4.py
@@ -1,6 +1,11 @@
-quarter = input('Введите номер четверти от 1 до 4: ')
 
-def Coordinate(quarter):
+
+def Coordinate():
+    quarter = input('Введите номер четверти от 1 до 4: ')
+    while not quarter.isdigit():
+        print('Это не число')
+        quarter = input('Введите номер четверти от 1 до 4: ')
+    quarter = int(quarter)
     if quarter == 1:
         print(f"В четверти №{quarter}: Х (от 0 до +∞), Y (от 0 до +∞)")
     elif quarter == 2:
@@ -10,12 +15,4 @@ def Coordinate(quarter):
     elif quarter == 4:
         print(f"В четверти №{quarter}: Х (от 0 до +∞), Y (от 0 до -∞)")
     else: print('Такой четверти нет.')
-Coordinate(quarter)
-if type(quarter) == int:
-    if quarter < 5 and quarter > 0:
-        Coordinate(quarter)
-    else:
-        print('Такой четверти не существует')
-        quit
-else:
-    print('Данное значение не подходит!')
+Coordinate()
