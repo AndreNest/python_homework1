aeca782982fd801f5815606080265bb13cb24079
AndreyNesterov <1@nestandre.ru>
2022-09-07 00:17:13 +0300

    v 1.1

--- task1.py
+++ task1.py
@@ -1,6 +1,14 @@
-day = int(input('Введите число от 1 до 7: '))
-if day > 5 and day < 8:
-    print(f'{day} - это выходной')
-elif day > 0 and day < 6:
-    print(f'{day} - это будний день')
-else: print('Не правильное значение')
+def day():
+    num = input('Введите число: ')
+    while num.isdigit() is not True:
+        print('Не правильное значение')
+        num = input('Введите число: ')
+    num = int(num)
+    if 0 < num < 6:
+        print("Это будний день")
+    elif 5 < num < 8:
+        print("Это выходной")
+    else:
+        print("такого дня не существует")
+
+day()
